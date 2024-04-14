9c8bbbea9b147bd43d0f0a17a4a7c1ce901a6561
peterpileta <escallobro@gmail.com>
2024-04-14 22:27:02 +0100

    Update LibraryApplication.java

--- LibraryApplication.java
+++ LibraryApplication.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 /**
- * Peter Pileta, Software Development I, 04/07/2024
  * Class Name: LibraryApplication
  * This class is the entry point for the Library Management System. 
  */
